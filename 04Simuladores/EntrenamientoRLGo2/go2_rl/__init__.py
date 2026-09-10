666d65909b2ea302949c6ed4c75d750b8384ef01
Fer <145226583+vallferre@users.noreply.github.com>
2026-09-10 18:56:00 -0300

    Delete 04Simuladores directory

--- 04Simuladores/EntrenamientoRLGo2/go2_rl/__init__.py
+++
@@ -1,3 +0,0 @@
-from .env import Go2WalkEnv, Go2WalkEnvConfig
-
-__all__ = ["Go2WalkEnv", "Go2WalkEnvConfig"]
--- 04Simuladores/UnitreeMujocoOficial/abrir_g1_sim.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_g1_sim.ps1" %*
-if errorlevel 1 pause
